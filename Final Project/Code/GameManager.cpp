4331aa44d0f7e92edb4cebcbff598caa621b73c6
Ryan Harris <harris.ucla@gmail.com>
2011-11-19 18:50:46 -0800

    updated objects

--- Final Project/Code/GameManager.cpp
+++ Final Project/Code/GameManager.cpp
@@ -518,6 +518,23 @@ void GameManager::RenderHUD()
 	SetupCamera(*m_player->getPosition());
 }
 
+std::string GameManager::intID(int x)
+{
+	switch(x) {
+	case 1: return "one"; break;
+	case 2: return "two"; break;
+	case 3: return "three"; break;
+	case 4: return "four"; break;
+	case 5: return "five"; break;
+	case 6: return "six"; break;
+	case 7: return "seven"; break;
+	case 8: return "eight"; break;
+	case 9: return "nine"; break;
+	case 0: return "zero"; break;
+	default: return "none";
+	}
+}
+
 directionType relativePosition(Object& a, Object& b)
 {
 	vec3* op = b.getPosition();
